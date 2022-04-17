0181ed6b0e21455b58e1ed4246a66899a0d9b85c
Manish Gautam <mayankgautam2105@gmail.com>
2022-04-17 23:46:16 +0530

    updates

--- lru.cpp
+++ lru.cpp
@@ -1,76 +1,163 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
 
+// using namespace std;
+
+// int main()
+// {
+//     int fs, ref_str[100], n, faults = 0, hits = 0, frm_cntr = 0, far_index, res;
+//     cout << "Frame size: \n";
+//     vector<int> q;
+
+//     cin >> fs;
+//     cout << "Page Size: \n";
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> ref_str[i];
+//     }
+
+//     bool init_flag = true;
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         if (q.size() < fs)
+//         {
+//             faults++;
+//             q.push_back(ref_str[i]);
+//         }
+//         else
+//         {
+//             if (find(q.begin(), q.end(), ref_str[i]) != q.end())
+//             {
+//                 hits++;
+//             }
+//             else
+//             {
+
+//                 far_index, res = -1;
+//                 for (int m = 0; m < fs; m++)
+//                 {
+//                     for (int j = 0; j < i; j++)
+//                     {
+//                         if (q[m] == ref_str[j])
+//                         {
+//                             if (far_index > j)
+//                             {
+//                                 far_index = j;
+//                                 res = 1;
+//                             }
+//                         }
+//                     }
+//                 }
+//                 if (!res)
+//                 {
+//                     q[far_index] = ref_str[i];
+//                 }
+//                 else
+//                 {
+//                     q[0] = ref_str[i];
+//                 }
+
+//                 frm_cntr++;
+//                 frm_cntr = frm_cntr % fs;
+//                 faults++;
+//             };
+//         }
+//     }
+
+//     cout << "Hits: " << hits << endl;
+//     cout << "Faults: "
+//          << faults;
+
+//     return 0;
+// }
+
+// C++ implementation of above algorithm
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
+// Function to find page faults using indexes
+int pageFaults(int pages[], int n, int capacity)
 {
-    int fs, ref_str[100], n, faults = 0, hits = 0, frm_cntr = 0, far_index, res;
-    cout << "Frame size: \n";
-    vector<int> q;
+    // To represent set of current pages. We use
+    // an unordered_set so that we quickly check
+    // if a page is present in set or not
+    unordered_set<int> s;
 
-    cin >> fs;
-    cout << "Page Size: \n";
-    cin >> n;
+    // To store least recently used indexes
+    // of pages.
+    unordered_map<int, int> indexes;
+
+    // Start from initial page
+    int page_faults = 0;
     for (int i = 0; i < n; i++)
     {
-        cin >> ref_str[i];
-    }
-
-    bool init_flag = true;
-
-    for (int i = 0; i < n; i++)
-    {
-
-        if (q.size() < fs)
+        // Check if the set can hold more pages
+        if (s.size() < capacity)
         {
-            faults++;
-            q.push_back(ref_str[i]);
+            // Insert it into set if not present
+            // already which represents page fault
+            if (s.find(pages[i]) == s.end())
+            {
+                s.insert(pages[i]);
+
+                // increment page fault
+                page_faults++;
+            }
+
+            // Store the recently used index of
+            // each page
+            indexes[pages[i]] = i;
         }
+
+        // If the set is full then need to perform lru
+        // i.e. remove the least recently used page
+        // and insert the current page
         else
         {
-            if (find(q.begin(), q.end(), ref_str[i]) != q.end())
+            // Check if current page is not already
+            // present in the set
+            if (s.find(pages[i]) == s.end())
             {
-                hits++;
-            }
-            else
-            {
-
-                far_index, res = -1;
-                for (int m = 0; m < fs; m++)
+                // Find the least recently used pages
+                // that is present in the set
+                int lru = INT_MAX, val;
+                for (auto it = s.begin(); it != s.end(); it++)
                 {
-                    for (int j = 0; j < i; j++)
+                    if (indexes[*it] < lru)
                     {
-                        if (q[j] == ref_str[j])
-                        {
-                            if (far_index > j)
-                            {
-                                far_index = j;
-                                res = 1;
-                            }
-                        }
+                        lru = indexes[*it];
+                        val = *it;
                     }
                 }
-                if (!res)
-                {
-                    q[far_index] = ref_str[i];
-                }
-                else
-                {
-                    q[0] = ref_str[i];
-                }
 
-                frm_cntr++;
-                frm_cntr = frm_cntr % fs;
-                faults++;
-            };
+                // Remove the indexes page
+                s.erase(val);
+
+                // insert the current page
+                s.insert(pages[i]);
+
+                // Increment page faults
+                page_faults++;
+            }
+
+            // Update the current page index
+            indexes[pages[i]] = i;
         }
     }
 
-    cout << "Hits: " << hits << endl;
-    cout << "Faults: "
-         << faults;
+    return page_faults;
+}
 
+// Driver code
+int main()
+{
+    int pages[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2};
+    int n = sizeof(pages) / sizeof(pages[0]);
+    int capacity = 4;
+    cout << pageFaults(pages, n, capacity);
     return 0;
 }
--- optimal_page_replacement.cpp
+++ optimal_page_replacement.cpp
@@ -1,76 +1,172 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
 
+// using namespace std;
+
+// int main()
+// {
+//     int fs, ref_str[100], n, faults = 0, hits = 0, frm_cntr = 0, far_index, res;
+//     cout << "Frame size: \n";
+//     vector<int> q;
+
+//     cin >> fs;
+//     cout << "Page Size: \n";
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> ref_str[i];
+//     }
+
+//     bool init_flag = true;
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         if (q.size() < fs)
+//         {
+//             faults++;
+//             q.push_back(ref_str[i]);
+//         }
+//         else
+//         {
+//             if (find(q.begin(), q.end(), ref_str[i]) != q.end())
+//             {
+//                 hits++;
+//             }
+//             else
+//             {
+
+//                 far_index, res = -1;
+//                 for (int m = 0; m < fs; m++)
+//                 {
+//                     for (int j = i; j < n; j++)
+//                     {
+//                         if (q[m] == ref_str[j])
+//                         {
+//                             if (far_index > j)
+//                             {
+//                                 far_index = j;
+//                                 res = 1;
+//                             }
+//                         }
+//                     }
+//                 }
+//                 if (!res)
+//                 {
+//                     q[far_index] = ref_str[i];
+//                 }
+//                 else
+//                 {
+//                     q[0] = ref_str[i];
+//                 }
+
+//                 frm_cntr++;
+//                 frm_cntr = frm_cntr % fs;
+//                 faults++;
+//             }
+//         }
+//     }
+
+//     cout << "Hits" << hits << endl;
+//     cout << "Faults \n"
+//          << faults;
+
+//     return 0;
+// }
+
+// CPP program to demonstrate optimal page
+// replacement algorithm.
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
+// Function to check whether a page exists
+// in a frame or not
+bool search(int key, vector<int> &fr)
 {
-    int fs, ref_str[100], n, faults = 0, hits = 0, frm_cntr = 0, far_index, res;
-    cout << "Frame size: \n";
-    vector<int> q;
+    for (int i = 0; i < fr.size(); i++)
+        if (fr[i] == key)
+            return true;
+    return false;
+}
 
-    cin >> fs;
-    cout << "Page Size: \n";
-    cin >> n;
-    for (int i = 0; i < n; i++)
+// Function to find the frame that will not be used
+// recently in future after given index in pg[0..pn-1]
+int predict(int pg[], vector<int> &fr, int pn, int index)
+{
+    // Store the index of pages which are going
+    // to be used recently in future
+    int res = -1, farthest = index;
+    for (int i = 0; i < fr.size(); i++)
     {
-        cin >> ref_str[i];
+        int j;
+        for (j = index; j < pn; j++)
+        {
+            if (fr[i] == pg[j])
+            {
+                if (j > farthest)
+                {
+                    farthest = j;
+                    res = i;
+                }
+                break;
+            }
+        }
+
+        // If a page is never referenced in future,
+        // return it.
+        if (j == pn)
+            return i;
     }
 
-    bool init_flag = true;
+    // If all of the frames were not in future,
+    // return any of them, we return 0. Otherwise
+    // we return res.
+    return (res == -1) ? 0 : res;
+}
 
-    for (int i = 0; i < n; i++)
+void optimalPage(int pg[], int pn, int fn)
+{
+    // Create an array for given number of
+    // frames and initialize it as empty.
+    vector<int> fr;
+
+    // Traverse through page reference array
+    // and check for miss and hit.
+    int hit = 0;
+    for (int i = 0; i < pn; i++)
     {
 
-        if (q.size() < fs)
+        // Page found in a frame : HIT
+        if (search(pg[i], fr))
         {
-            faults++;
-            q.push_back(ref_str[i]);
+            hit++;
+            continue;
         }
+
+        // Page not found in a frame : MISS
+
+        // If there is space available in frames.
+        if (fr.size() < fn)
+            fr.push_back(pg[i]);
+
+        // Find the page to be replaced.
         else
         {
-            if (find(q.begin(), q.end(), ref_str[i]) != q.end())
-            {
-                hits++;
-            }
-            else
-            {
-
-                far_index, res = -1;
-                for (int m = 0; m < fs; m++)
-                {
-                    for (int j = i; j < n; j++)
-                    {
-                        if (q[j] == ref_str[j])
-                        {
-                            if (far_index > j)
-                            {
-                                far_index = j;
-                                res = 1;
-                            }
-                        }
-                    }
-                }
-                if (!res)
-                {
-                    q[far_index] = ref_str[i];
-                }
-                else
-                {
-                    q[0] = ref_str[i];
-                }
-
-                frm_cntr++;
-                frm_cntr = frm_cntr % fs;
-                faults++;
-            };
+            int j = predict(pg, fr, pn, i + 1);
+            fr[j] = pg[i];
         }
     }
+    cout << "No. of hits = " << hit << endl;
+    cout << "No. of misses = " << pn - hit << endl;
+}
 
-    cout << "Hits" << hits << endl;
-    cout << "Faults \n"
-         << faults;
-
+// Driver Function
+int main()
+{
+    int pg[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2};
+    int pn = sizeof(pg) / sizeof(pg[0]);
+    int fn = 4;
+    optimalPage(pg, pn, fn);
     return 0;
 }
